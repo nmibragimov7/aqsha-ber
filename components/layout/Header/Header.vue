041e32fa06ab22cfbf6f73dfef934831f8823a3d
nibragimov <nibragimov@1cb.kz>
2022-03-09 16:47:38 +0600

    add pop up menu

--- components/layout/Header/Header.vue
+++ components/layout/Header/Header.vue
@@ -4,19 +4,47 @@
       <NuxtLink to='/' class='header__logo'>
         <img src='~/assets/images/logo.svg' alt='Logo' />
       </NuxtLink>
-      <button class='header__bars'></button>
+      <div class='header__bars'
+           @click='barsHandler'></div>
+      <div class='header__pop-up-menu' :class='[{"header__pop-up-menu--active": isOpen}]'>
+        <div class='header__bars header__bars--active'
+             @click='barsHandler'></div>
+        <NuxtLink to='/' class='header__pop-up-menu--logo'>
+          <img src='~/assets/images/logo-small.svg' alt='Logo' />
+        </NuxtLink>
+        <div class='container'>
+          <NuxtLink to='/' class='header__pop-up-menu--link'>
+            <p class='m-0 mb-4'>Главная</p>
+          </NuxtLink>
+          <NuxtLink to='/' class='header__pop-up-menu--link'>
+            <p class='m-0 mb-4'>Способы получения и погашения</p>
+          </NuxtLink>
+          <NuxtLink to='/' class='header__pop-up-menu--link'>
+            <p class='m-0 mb-4'>Документы</p>
+          </NuxtLink>
+          <NuxtLink to='/' class='header__pop-up-menu--link'>
+            <p class='m-0 mb-4'>Контакты</p>
+          </NuxtLink>
+          <hr class='mb-4'>
+          <div>
+            <nuxt-link class='header__pop-up-menu--link' to='/login'>Вход</nuxt-link>
+            <span class='header__pop-up-menu--link'>/</span>
+            <nuxt-link class='header__pop-up-menu--link' to='/register'>Регистрация</nuxt-link>
+          </div>
+        </div>
+      </div>
       <div class='header__item'>
         <div class='header__navs flex-grow-1 d-flex'>
           <NuxtLink v-for='(menu, index) in menus'
                     :key='index'
                     :to='menu.path'
-                    class='header__nav p-2 ml-2'>
+                    class='header__link p-2 ml-2'>
             {{ menu.title }}
           </NuxtLink>
-          <div class='header__signBtns p-2 ml-2'>
-            <nuxt-link class='header__nav' to='/login'>Вход</nuxt-link>
-            <span class='header__nav'>/</span>
-            <nuxt-link class='header__nav' to='/register'>Регистрация</nuxt-link>
+          <div class='p-2 ml-2'>
+            <nuxt-link class='header__link' to='/login'>Вход</nuxt-link>
+            <span class='header__link'>/</span>
+            <nuxt-link class='header__link' to='/register'>Регистрация</nuxt-link>
           </div>
         </div>
       </div>
@@ -31,7 +59,13 @@ export default {
   name: 'Header',
   data() {
     return {
-      menus
+      menus,
+      isOpen: false
+    }
+  },
+  methods: {
+    barsHandler() {
+      this.isOpen = !this.isOpen;
     }
   }
 }
@@ -54,6 +88,38 @@ export default {
     }
   }
 
+  &__pop-up-menu {
+    position: fixed;
+    top: 0;
+    right: 0;
+    left: 0;
+    background: #F8F9F9;
+    border-radius: 0 0 30px 30px;
+    height: 50vh;
+    -webkit-transition: 0.2s ease-in-out;
+    transition: 0.2s ease-in-out;
+    transform: translateY(-100%);
+    padding: 80px 20px 0 20px;
+    @media (min-width: 900px) {
+      display: none;
+    }
+
+    &--active {
+      transform: translateY(0%);
+    }
+
+    &--logo {
+      position: absolute;
+      top: 5px;
+      right: 26px;
+    }
+
+    &--link {
+      text-decoration: none;
+      color: #322443;
+    }
+  }
+
   &__bars {
     cursor: pointer;
     position: absolute;
@@ -65,6 +131,18 @@ export default {
     background-color: #FFF;
     -webkit-transition: 0.2s ease-in-out;
     transition: 0.2s ease-in-out;
+
+    &--active {
+      top: 31px;
+      left: 26px;
+      background-color: #322443;
+      transform: rotate(90deg);
+
+      &::before,
+      &::after {
+        background-color: #322443 !important;
+      }
+    }
 
     &::before,
     &::after {
@@ -94,13 +172,13 @@ export default {
     display: none;
   }
 
-  &__nav {
+  &__link {
     text-decoration: underline;
     color: #FFF;
     font-size: 24px;
   }
 
-  @media (min-width: 768px) {
+  @media (min-width: 900px) {
     &__container {
       justify-content: space-between;
     }
